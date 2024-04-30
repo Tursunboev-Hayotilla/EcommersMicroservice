6e6516c320fd154cfe2b2a75e2056f78791469df
Hayotilla Tursunboev <coder0227@gmail.com>
2024-05-01 00:42:52 +0300

    models created

---
+++ src/Ordering/Orders.Domain/Entities/OrderModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Domain.Entities
+{
+    public class OrderModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid ProductId { get; set; } = Guid.NewGuid();
+        public Guid UserId {  get; set; } = Guid.NewGuid();
+        public int ProductCount { get; set; }
+        public DateTimeOffset OrderedDate {  get; set; } = DateTimeOffset.UtcNow;
+    }
+}
---
+++ src/Ordering/Orders.Domain/Entities/ResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Domain.Entities
+{
+    public class ResponseModel
+    {
+        public string Message {  get; set; }
+        public string StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}

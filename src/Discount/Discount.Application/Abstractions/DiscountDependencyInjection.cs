631f63824a978f5e62c044790cc1416c1e1d3310
Hayotilla Tursunboev <coder0227@gmail.com>
2024-04-30 16:15:07 +0300

    Models created

---
+++ src/Discount/Discount.Application/Abstractions/DiscountDependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.Application.Abstractions
+{
+    public static class DiscountDependencyInjection
+    {
+        public static IServiceCollection AddDiscountApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}   
